996734162574f27a094c38d5dcde4cc679469ae1
itsmanik <manik.hilalpure@gmail.com>
2024-06-05 18:08:32 +0530

    added login feature using clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
-}
+import {
+    SignedIn,
+    SignedOut,
+    UserButton,
+    SignInButton,
+    SignUpButton
+} from "@clerk/clerk-react";
 
-export default App
+const App = () => {
+    return (
+        <div className="font-serif">
+            <SignedOut>
+                <SignInButton mode="modal" className="border p-3 bg-gray-950 text-white" />
+                <SignUpButton className="border p-3 bg-gray-950 text-white" mode="modal"/>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </div>
+    );
+};
+
+export default App;
